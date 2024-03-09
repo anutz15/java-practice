0e61a5940fd7b659f1727e9af5dc7816fa0c04b0
anutz15 <anujshah120102@gmail.com>
2024-03-09 22:12:37 +0530

    new files added

--- StringPractice/src/String2.java
+++ StringPractice/src/String2.java
@@ -1,9 +1,11 @@
 public class String2 {
     public static void main(String[] args) {
-        float a=123.219f;
+        // float a=123.219f;
         // System.out.printf("Formatted number is: %.2f", a);  // it rounds of as well
 
         // pi value till 3 decimal places
-        System.out.printf("Value: %.3f",Math.PI);
+        System.out.printf("Value: %.3f\n",Math.PI);
+        System.out.println('a'+'b');    // this will print 195, as it will add ACII values of a,b
+        System.out.println("a"+"b");    // this will print ab
     }
 }
